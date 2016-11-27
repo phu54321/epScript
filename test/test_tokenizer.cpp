9b4b35e3768fa73081013e8933eb58afe5fe4bea
phu54321 <phu54321@naver.com>
2016-11-27 19:29:35 +0900

    Added comparator tests

--- test/test_tokenizer.cpp
+++ test/test_tokenizer.cpp
@@ -11,3 +11,16 @@ TEST_CASE("Tokenizing arithmetic operators") {
     REQUIRE(tok.getToken()->type == TOKEN_DIVIDE);
     REQUIRE(tok.getToken() == nullptr);
 }
+
+
+TEST_CASE("Tokenizing comparison operators") {
+    std::stringstream test_input("==!=<=>=<>");
+    Tokenizer tok(test_input);
+    REQUIRE(tok.getToken()->type == TOKEN_EQ);
+    REQUIRE(tok.getToken()->type == TOKEN_NE);
+    REQUIRE(tok.getToken()->type == TOKEN_LE);
+    REQUIRE(tok.getToken()->type == TOKEN_GE);
+    REQUIRE(tok.getToken()->type == TOKEN_LT);
+    REQUIRE(tok.getToken()->type == TOKEN_GT);
+    REQUIRE(tok.getToken() == nullptr);
+}
