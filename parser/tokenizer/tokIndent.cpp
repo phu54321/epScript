47b18b1ed85a01b70b3e1452725e29ab7d8d7a84
phu54321 <phu54321@naver.com>
2017-12-13 11:18:40 +0900

    Merge branch 'develop' into feature_object - Use catch framework (3)

--- parser/tokenizer/tokIndent.cpp
+++ parser/tokenizer/tokIndent.cpp
@@ -66,33 +66,3 @@ void TokIndent::unindent() {
 int TokIndent::getLastIndentLevel() const {
     return indentStacks[indentStacks.size() - 1];
 }
-
-
-TEST_CASE("Indent use case") {
-    TokIndent indenter;
-
-    REQUIRE_THROWS_AS(indenter.setCurrentLineIndent(-1), std::domain_error);
-
-
-    indenter.setCurrentLineIndent(4);
-            REQUIRE(indenter.remainingIndentTokens() > 0);
-    indenter.indent();
-            REQUIRE(indenter.remainingIndentTokens() == 0);
-            REQUIRE_THROWS_AS(indenter.indent(), std::logic_error);  // Indenting occur.
-
-    indenter.setCurrentLineIndent(4);
-            REQUIRE(indenter.remainingIndentTokens() == 0);
-
-    indenter.setCurrentLineIndent(8);
-            REQUIRE(indenter.remainingIndentTokens() > 0);
-    indenter.indent();
-            REQUIRE(indenter.remainingIndentTokens() == 0);
-
-    indenter.setCurrentLineIndent(0);
-            REQUIRE(indenter.remainingIndentTokens() < 0);
-            REQUIRE_THROWS_AS(indenter.setCurrentLineIndent(4), std::logic_error);  // Indent is not yet resolved.
-    indenter.unindent();
-            REQUIRE(indenter.remainingIndentTokens() < 0);
-    indenter.unindent();
-            REQUIRE(indenter.remainingIndentTokens() == 0);
-}
