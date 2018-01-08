17457fd67ff913e6b1c6684a3519cf3464966e8b
phu54321 <phu54321@naver.com>
2018-01-08 19:58:27 +0900

    Split globalv tests

---
+++ test/parser/test_parser_globalv.cpp
@@ -0,0 +1,14 @@
+//
+// Created by phu54321 on 2018-01-08.
+//
+
+#include "test_base.hpp"
+
+TEST_CASE("Multiple variable") {
+    check_string(
+            "var a, b = 1, 2;",
+
+            "a, b = EUDCreateVariables(2)\n"
+                    "_IGVA([a, b], lambda: [1, 2])\n"
+    );
+}
