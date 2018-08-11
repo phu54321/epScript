a53ad5111e84f376ab50f8a88e941154240fbe5a
phu54321 <phu54321@naver.com>
2018-08-12 04:49:10 +0900

    Fixes #4

--- test/parser/test_bugcase_4.cpp
+++ test/parser/test_bugcase_4.cpp
@@ -2,15 +2,19 @@
 // Created by phu54321 on 2018-08-12.
 //
 
-#include "test_base.hpp"
+#include "../test_base.hpp"
 
 TEST_CASE("Bug case #4") {
-    CHECK((ParseString("Test", "function afterTriggerExec() {\n"
-                               "    var a;\n"
-                               "    if(a == 0) {\n"
-                               "        a++;\n"
-                               "    }\n"
-                               "    dbstr_print(\"lol\"\n"
-                               "    dbstr_print(\"lol\")\n"
-                               "}"), getParseErrorNum() > 0));
+    checkError("function afterTriggerExec() {\n"
+               "    var a;\n"
+               "    (\n"
+               "}");
+    checkError("function afterTriggerExec() {\n"
+               "    var a;\n"
+               "    if(a == 0) {\n"
+               "        a++;\n"
+               "    }\n"
+               "    dbstr_print(\"lol\"\n"
+               "    dbstr_print(\"lol\")\n"
+               "}");
 }
